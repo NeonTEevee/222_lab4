44583ae24dd8fcbb5e6777bfb3a51df5d7e176a8
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-04 17:28:49 +0700

    I got nothing on last item qwq

---
+++ References.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int i;
+	char first[50], second[50], third[50], fourth[50];
+	scanf("%s", first);
+	scanf("%s", second);
+	scanf("%s", third);
+	scanf("%s", fourth);
+	
+	
+}
