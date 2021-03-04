d60c150d65821eed66e8347c5ec0e0aaef752b65
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-04 16:24:51 +0700

    Trying to figure out how to go about doing this

--- Stack.cpp
+++ Stack.cpp
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 int main ()
 {
-	int terms, stacks;
+	int i = 0, terms, stack[100], num;
 	char input[100];
-	scanf("%[^\n]", input);
+	scanf("%d", &terms);
+	scanf(" %[^\n]", input);
 	
 	char *token = strtok(input, " ");
 	while(token != NULL) {
-		printf("%s\n", token);
+		if(token == "+")
+		{
+			i--;
+			num = stack[i-1] + stack[i];
+			stack[i] = 0;
+		} else if (token == "-") {
+			i--;
+			num = stack[i-1] - stack[i];
+			stack[i] = 0;
+		} else if (token == "*") {
+			i--;
+			num = stack[i-1] * stack[i];
+			stack[i] = 0;
+		} else {
+			stack[i] = atoi(token);
+			printf("%d\n", stack[i]);
+			i++;
+		}
 		token = strtok(NULL, " ");
 	}
 }
