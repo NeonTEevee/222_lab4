14b356f83dcfe3ba0b19635b4528affbad86970c
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-04 14:59:09 +0700

    Stared working on item "Dirty"

---
+++ Dirty.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+	int i;
+	char word[200], result[200];
+	scanf("%s", word);
+	printf("Input: %s\n", word);
+	for(i=0; i<200; i++)
+	{
+			
+	}
+}
