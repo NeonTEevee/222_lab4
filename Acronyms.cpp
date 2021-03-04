5601e9fac1f810778a38243086e878dbb508a2a6
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-04 15:43:01 +0700

    Finished the program, now to check if it works

--- Acronyms.cpp
+++ Acronyms.cpp
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
+	int i, j = 1;
+	char sentence[200], acronym[200];
+	scanf("%s", sentence);
 	
+	acronym[0] = sentence[0] - 32;
+	for(i=1; i<strlen(sentence); i++)
+	{
+		if(sentence[i] == ' ')
+			acronym[j] = sentence[i+1] - 32;
+	}
+	
+	printf("%s", acronym);
+	return 0;
 }
