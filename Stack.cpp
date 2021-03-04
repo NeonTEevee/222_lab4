2a89b3a3c01a1e703aac34a734ab7221aee2a61a
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-04 16:12:08 +0700

    Checking stuff with strtok

--- Stack.cpp
+++ Stack.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
+	int terms, stacks;
+	char input[100];
+	scanf("%[^\n]", input);
 	
+	char *token = strtok(input, " ");
+	while(token != NULL) {
+		printf("%s\n", token);
+		token = strtok(NULL, " ");
+	}
 }
