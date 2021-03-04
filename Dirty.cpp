8abf46934552c0c662f15a64fe0096f53bd75370
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-04 15:05:55 +0700

    Removing the printf line that was there to check for errors

--- Dirty.cpp
+++ Dirty.cpp
@@ -2,17 +2,17 @@
 #include <string.h>
 int main ()
 {
-	int i;
+	int i, j = 0;
 	char word[200], result[200], tempt[200];
 	scanf("%s", word);
-	printf("Input: %s\n", word);
 	
 	result[0] = word[0];
 	for(i=1; i<200; i++)
 	{
-		if(word[i] != result[i-1])
+		if(word[i] != result[j])
 		{
-			
+			result[j+1] = word[i];
+			j++;
 		}
 	}
 	printf("%s", result);
